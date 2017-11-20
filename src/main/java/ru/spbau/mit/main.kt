8e2aea661e96a1a59df0bf5c008ff8603f265dfc
LabutinIgor <Labutin.IgorL@gmail.com>
2017-11-20 23:28:05 +0300

    Some fixes
    Signed-off-by: LabutinIgor <Labutin.IgorL@gmail.com>

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -4,27 +4,20 @@ import java.io.PrintWriter
 import java.util.*
 import kotlin.collections.ArrayList
 
-data class Vector(private val x: Long, private val y: Long, val id: Int? = null) : Comparable<Vector> {
-    override fun compareTo(other: Vector): Int {
-        if (this.x == other.x && this.y == other.y) {
-            return 0
-        }
-        if (y == 0L && other.y == 0L) {
-            return -x.compareTo(0)
-        }
-        if (y == 0L && x > 0) {
-            return -1
-        }
-        if (other.y == 0L && other.x > 0) {
-            return 1
-        }
-
-        val thisHalfPlane = -y.compareTo(0)
-        val otherHalfPlane = -other.y.compareTo(0)
-        return if (thisHalfPlane == otherHalfPlane) {
-            -this.cross(other).compareTo(0)
-        } else {
-            thisHalfPlane.compareTo(otherHalfPlane)
+data class Vector(private val x: Long, private val y: Long) : Comparable<Vector> {
+    override fun compareTo(other: Vector): Int = when {
+        x == other.x && y == other.y -> 0
+        y == 0L && other.y == 0L -> -x.compareTo(0)
+        y == 0L && x > 0 -> -1
+        other.y == 0L && other.x > 0 -> 1
+        else -> {
+            val thisHalfPlane = -y.compareTo(0)
+            val otherHalfPlane = -other.y.compareTo(0)
+            if (thisHalfPlane == otherHalfPlane) {
+                -cross(other).compareTo(0)
+            } else {
+                thisHalfPlane.compareTo(otherHalfPlane)
+            }
         }
     }
 
@@ -32,56 +25,51 @@ data class Vector(private val x: Long, private val y: Long, val id: Int? = null)
     fun cross(other: Vector) = x * other.y - y * other.x
 }
 
-class InputReader {
-    fun readData(): ArrayList<Vector> {
-        val data = ArrayList<Vector>()
-        val scanner = Scanner(System.`in`)
-        val n = scanner.nextInt()
-        for (i in 1..n) {
-            val x = scanner.nextLong()
-            val y = scanner.nextLong()
-            data.add(Vector(x, y, i))
+data class VectorWithId(val vector: Vector, val id: Int) : Comparable<VectorWithId> {
+    override fun compareTo(other: VectorWithId): Int = vector.compareTo(other.vector)
+}
+
+fun readData(): ArrayList<VectorWithId> {
+    val data = ArrayList<VectorWithId>()
+    val scanner = Scanner(System.`in`)
+    val n = scanner.nextInt()
+    for (i in 1..n) {
+        val x = scanner.nextLong()
+        val y = scanner.nextLong()
+        data.add(VectorWithId(Vector(x, y), i))
+    }
+    scanner.close()
+    return data
+}
+
+fun writeAnswer(ids: Pair<Int, Int>) {
+    val printWriter = PrintWriter(System.out)
+    printWriter.println(ids.first.toString() + " " + ids.second + "\n")
+    printWriter.close()
+}
+
+fun solve(data: ArrayList<VectorWithId>): Pair<Int, Int> {
+    val sortedPoints = data.sorted()
+    var resId = 0
+    for (i in 0 until sortedPoints.size) {
+        if (angleLess(sortedPoints[i].vector, sortedPoints[(i + 1) % sortedPoints.size].vector,
+                sortedPoints[resId].vector, sortedPoints[(resId + 1) % sortedPoints.size].vector)) {
+            resId = i
         }
-        scanner.close()
-        return data
     }
+    return Pair(sortedPoints[resId].id, sortedPoints[(resId + 1) % sortedPoints.size].id)
 }
 
-class OutputWriter {
-    fun writeAnswer(ids: Pair<Int, Int>) {
-        val printWriter = PrintWriter(System.out)
-        printWriter.println(ids.first.toString() + " " + ids.second + "\n")
-        printWriter.close()
-    }
+private fun angleLess(firstAngle1: Vector, firstAngle2: Vector,
+                      secondAngle1: Vector, secondAngle2: Vector): Boolean {
+    val p1 = Vector(firstAngle1.dot(firstAngle2), Math.abs(firstAngle1.cross(firstAngle2)))
+    val p2 = Vector(secondAngle1.dot(secondAngle2), Math.abs(secondAngle1.cross(secondAngle2)))
+    return p1.cross(p2) > 0
 }
 
-class Solution {
-    fun solve(data: ArrayList<Vector>): Pair<Int, Int> {
-        val sortedPoints = data.sorted()
-        var resId = 0
-        (0 until sortedPoints.size).asSequence().filter {
-                    angleLess(sortedPoints[it], sortedPoints[(it + 1) % sortedPoints.size],
-                            sortedPoints[resId], sortedPoints[(resId + 1) % sortedPoints.size])
-                }
-                .forEach { resId = it }
-        return Pair(sortedPoints[resId].id!!, sortedPoints[(resId + 1) % sortedPoints.size].id!!)
-    }
-
-    private fun angleLess(firstAngle1: Vector, firstAngle2: Vector,
-                          secondAngle1: Vector, secondAngle2: Vector): Boolean {
-        val p1 = Vector(firstAngle1.dot(firstAngle2), Math.abs(firstAngle1.cross(firstAngle2)))
-        val p2 = Vector(secondAngle1.dot(secondAngle2), Math.abs(secondAngle1.cross(secondAngle2)))
-        return p1.cross(p2) > 0
-    }
-}
 
 fun main(args: Array<String>) {
-    val reader = InputReader()
-    val data = reader.readData()
-
-    val solution = Solution()
-    val answer = solution.solve(data)
-
-    val writer = OutputWriter()
-    writer.writeAnswer(answer)
+    val data = readData()
+    val answer = solve(data)
+    writeAnswer(answer)
 }
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -5,27 +5,25 @@ import org.junit.Test
 class TestSource {
     @Test
     fun testSample1() {
-        val solution = Solution()
-        val inputVectors = ArrayList<Vector>()
-        inputVectors.add(Vector(-1, 0, 1))
-        inputVectors.add(Vector(0, -1, 2))
-        inputVectors.add(Vector(1, 0, 3))
-        inputVectors.add(Vector(1, 1, 4))
-        val res = solution.solve(inputVectors)
+        val inputVectors = ArrayList<VectorWithId>()
+        inputVectors.add(VectorWithId(Vector(-1, 0), 1))
+        inputVectors.add(VectorWithId(Vector(0, -1), 2))
+        inputVectors.add(VectorWithId(Vector(1, 0), 3))
+        inputVectors.add(VectorWithId(Vector(1, 1), 4))
+        val res = solve(inputVectors)
         assertEquals(res, Pair(3, 4))
     }
 
     @Test
     fun testSample2() {
-        val solution = Solution()
-        val inputVectors = ArrayList<Vector>()
-        inputVectors.add(Vector(-1, 0, 1))
-        inputVectors.add(Vector(0, -1, 2))
-        inputVectors.add(Vector(1, 0, 3))
-        inputVectors.add(Vector(1, 1, 4))
-        inputVectors.add(Vector(-4, -5, 5))
-        inputVectors.add(Vector(-4, -6, 6))
-        val res = solution.solve(inputVectors)
+        val inputVectors = ArrayList<VectorWithId>()
+        inputVectors.add(VectorWithId(Vector(-1, 0), 1))
+        inputVectors.add(VectorWithId(Vector(0, -1), 2))
+        inputVectors.add(VectorWithId(Vector(1, 0), 3))
+        inputVectors.add(VectorWithId(Vector(1, 1), 4))
+        inputVectors.add(VectorWithId(Vector(-4, -5), 5))
+        inputVectors.add(VectorWithId(Vector(-4, -6), 6))
+        val res = solve(inputVectors)
         assertEquals(res, Pair(5, 6))
     }
 }
