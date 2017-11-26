aa8a0874dc57e6013eca02996b09d97913d5be99
LabutinIgor <Labutin.IgorL@gmail.com>
2017-11-26 20:11:54 +0300

    Some fixes
    Signed-off-by: LabutinIgor <Labutin.IgorL@gmail.com>

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,10 +1,9 @@
 package ru.spbau.mit
 
-import java.io.PrintWriter
 import java.util.*
 import kotlin.collections.ArrayList
 
-data class Vector(private val x: Long, private val y: Long) : Comparable<Vector> {
+class Vector(private val x: Long, private val y: Long) : Comparable<Vector> {
     override fun compareTo(other: Vector): Int = when {
         x == other.x && y == other.y -> 0
         y == 0L && other.y == 0L -> -x.compareTo(0)
@@ -25,7 +24,7 @@ data class Vector(private val x: Long, private val y: Long) : Comparable<Vector>
     fun cross(other: Vector) = x * other.y - y * other.x
 }
 
-data class VectorWithId(val vector: Vector, val id: Int) : Comparable<VectorWithId> {
+class VectorWithId(val vector: Vector, val id: Int) : Comparable<VectorWithId> {
     override fun compareTo(other: VectorWithId): Int = vector.compareTo(other.vector)
 }
 
@@ -43,27 +42,34 @@ fun readData(): List<VectorWithId> {
 }
 
 fun writeAnswer(ids: Pair<Int, Int>) {
-    val printWriter = PrintWriter(System.out)
-    printWriter.println(ids.first.toString() + " " + ids.second + "\n")
-    printWriter.close()
+    println(ids.first.toString() + " " + ids.second + "\n")
 }
 
 fun solve(data: List<VectorWithId>): Pair<Int, Int> {
     val sortedPoints = data.sorted()
+    val vectors = constructAnglesVectors(sortedPoints)
+    val resId = findMinAngleId(vectors)
+    return Pair(sortedPoints[resId].id, sortedPoints[(resId + 1) % sortedPoints.size].id)
+}
+
+fun constructAnglesVectors(sortedPoints: List<VectorWithId>): List<Vector> {
     val vectors: MutableList<Vector> = mutableListOf()
     for (i in 0 until sortedPoints.size) {
         val p1 = sortedPoints[i].vector
         val p2 = sortedPoints[(i + 1) % sortedPoints.size].vector
         vectors.add(Vector(p1.dot(p2), Math.abs(p1.cross(p2))))
     }
+    return vectors
+}
 
+fun findMinAngleId(vectors: List<Vector>): Int {
     var resId = 0
-    for (i in 0 until sortedPoints.size) {
+    for (i in 0 until vectors.size) {
         if (vectors[i].cross(vectors[resId]) > 0) {
             resId = i
         }
     }
-    return Pair(sortedPoints[resId].id, sortedPoints[(resId + 1) % sortedPoints.size].id)
+    return resId
 }
 
 fun main(args: Array<String>) {
